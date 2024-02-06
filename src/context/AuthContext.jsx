23b8720211384890f8c2c3e3245ff826d166e020
LilianBrombacher <lbrombacher91@gmail.com>
2024-02-06 16:02:32 +0100

    stoeien met token

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,4 +1,9 @@
-import React, {createContext, useState} from 'react';
+import React, {createContext, useEffect, useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from "axios";
+import {jwtDecode} from 'jwt-decode';
+import isTokenValid from "../components/helpers/isTokenValid";
+
 
 export const AuthContext = createContext({});
 
@@ -6,28 +11,103 @@ export function AuthContextProvider ({ children } ) {
     const [isAuth, setIsAuth] = useState({
         isAuth: false,
         user: null,
+        status: 'pending',
     });
 
-    const login = () => {
-        setIsAuth(true);
-        console.log('Gebruiker is ingelogd')
+
+    //mounting effect
+    useEffect(() => {
+        //haal de JWT op uit de local storage
+        const token = localStorage.getItem('token');
+
+        //als er wel een token is, haal dan opnieuw de gebruikersdata op
+        if (token && isTokenValid(token)) {
+            void login (token);
+        } else {
+            //als er geen token is doen we niks en zetten we de status op 'done'
+            setIsAuth({
+                isAuth: false,
+                user: null,
+                status: 'done',
+            });
+        }
+    }, []);
+    const navigate = useNavigate();
+
+    async function login(token) {
+        // Token moet in de local storage opgeslagen worden, want die hebben we vanaf nu vaker nodig!
+        localStorage.setItem('token', token);
+        // Om erachter te komen van wie deze token is, zodat we informatie op kunnen halen, decoderen we deze token
+        const decodedToken = jwtDecode(token);
+        // Op de gedecodeerde token vinden we de id van de gebruiker, die we kunnen gebruiken voor het request
+        console.log(decodedToken.sub);
+
+        try {
+            // probeer de informatie over deze gebruiker op te halen
+            const response = await axios.get(`http://localhost:3000/600/users/${decodedToken.sub}`, {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                }
+            });
+            // Wat voor informatie krijgen we terug?
+            console.log(response.data);
+
+            // als we die info hebben, sla het dan op in de state zodat iedereen er gebruik van kan maken:
+            setIsAuth({
+                isAuth: true,
+                user: {
+                    username: response.data.username,
+                    email: response.data.email,
+                    id: response.data.id,
+                },
+                status: 'done',
+            });
+        } catch (e) {
+            // Mislukt? Bah. Log de gebruiker dan maar weer uit!
+            logout();
+        }
+        console.log('Gebruiker is ingelogd!');
+        navigate('/profile');
+    }
+
+    function logout() {
+        console.log('Gebruiker is uitgelogd!');
+        setIsAuth({
+            isAuth: false,
+            user: null,
+            status: 'done',
+        });
+        navigate('/');
+    }
+
+    const contextData = {
+        isAuth: isAuth.isAuth,
+        logout: logout,
+        login: login,
     };
 
-    const logout = () => {
-        setIsAuth(false);
-        console.log('Gebruiker is uitgelogd')
-    };
-    const data = {
-        user: 'testuser',
-        email: 'testuser@gmail.com',
-        login: login,
-        logout: logout,
-        isAuth: isAuth,
-    };
+
+        // const login = () => {
+    //     setIsAuth(true);
+    //     console.log('Gebruiker is ingelogd')
+    // };
+    //
+    // const logout = () => {
+    //     setIsAuth(false);
+    //     console.log('Gebruiker is uitgelogd')
+    // };
+    // const data = {
+    //     user: 'testuser',
+    //     email: 'testuser@gmail.com',
+    //     login: login,
+    //     logout: logout,
+    //     isAuth: isAuth,
+    // };
 
     return (
-        <AuthContext.Provider value={data}>
-            {children}
+        <AuthContext.Provider value={contextData}>
+            {isAuth.status === 'done' ? children : <p>Loading..</p>}
         </AuthContext.Provider>
     );
 }
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,18 +1,39 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {Link, useNavigate} from 'react-router-dom';
 import { AuthContext } from "../context/AuthContext";
+import axios from "axios";
 
 function SignIn() {
-    const {isAuth, logout, login} = useContext(AuthContext)
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, toggleError] = useState(false);
+    const {isAuth, logout, login} = useContext(AuthContext);
     const navigate = useNavigate();
+    const baseUrl = 'http://localhost:3000';
 
     useEffect(() => {
         console.log(isAuth)
     }, [isAuth]);
 
-    function handleSubmit(e) {
+     async function handleSubmit(e) {
         e.preventDefault();
-        login();
+        // login();
+         toggleError(false);
+
+         try {
+             const result = await axios.post(baseUrl + '/login',{
+                 email: email,
+                 password: password,
+             });
+             //log resultaat in console
+             console.log(result.data);
+
+             //geef JWT token mee aan de login-functie van de context
+             login(result.data.accessToken);
+         } catch(e) {
+             console.error(e);
+             toggleError(true);
+         }
     }
 
   return (
@@ -21,7 +42,28 @@ function SignIn() {
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias cum debitis dolor dolore fuga id molestias qui quo unde?</p>
 
       <form onSubmit={handleSubmit}>
-        <p>*invoervelden*</p>
+        <label htmlFor="email-field">
+            Emailadres:
+            <input
+                type="email"
+                id="email-field"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                />
+        </label>
+        <label htmlFor="password-field">
+            Wachtwoord:
+            <input
+                type="password"
+                id="password-field"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                />
+        </label>
+          {error && <p className="error">Combinatie van emailadres en wachtwoord is onjuist</p>}
+
         <button
             type= "button"
             onClick={() => login(navigate("/profile"))}>
